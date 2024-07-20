499b20c2234810e4dae0f0478c063f70b1188050
Dan Murphy <43690299+devbydan@users.noreply.github.com>
2024-07-20 15:16:22 -0700

    Update Projects.js descriptions

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
               imgPath={starlink}
               isBlog={false}
               title="Starlink Analysis"
-              description="EDA project which orchestrates a holistic comparison and assessment of the reliability and performance attributes intrinsic to Wi-Fi, Ethernet, and Starlink networks within the domain of computer networks."
+              description="EDA project which orchestrates a holistic comparison and assessment of the reliability and performance attributes intrinsic to Wi-Fi, Ethernet, and Starlink networks within the domain of computer networks. This solution seeks to provide clarity by gathering empirical data, conducting systematic data analysis, validate inferences through comparative evaluation, and provide actionable insights to improve network performance in future work."
               ghLink="https://github.com/devbydan/starlink-analysis"
             />
           </Col>
@@ -40,7 +40,7 @@ function Projects() {
               imgPath={covid}
               isBlog={false}
               title="COVID-19 Data Analysis"
-              description="Big Data project used to view quantifiable & statistical COVID-19 data from the World Health Organization and Global Data Change Lab using Apache Spark"
+              description="Big Data project used to view quantifiable & statistical COVID-19 data from the World Health Organization and Global Data Change Lab using Apache Spark, Java and PSQL. This system is used to track information about how COVID-19 will spread; its percentages of positive, negative and inconclusive results in regions, countries, etc; predicting trends with live data, and much more."
               ghLink="https://github.com/devbydan/COVID19-Data-Analysis"          
             />
           </Col>
