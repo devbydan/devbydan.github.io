f799170e593dcbb7e39fbf299bebed558570eee4
Dan Murphy <43690299+devbydan@users.noreply.github.com>
2024-11-11 11:13:01 -0800

    Update resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../../src/Assets/MSCS_Resume_Murphy-Dan.pdf";
+import pdf from "../../../src/Assets/devbydan_resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
